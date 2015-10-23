b9685b24a3bdb303798ae08044a8dd6d3f43b34f
António Ferreira <metal_antonio@hotmail.com>
2015-10-23 02:09:18 +0100

    started working on broker.

--- Broker/Program.cs
+++ Broker/Program.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommonTypes;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 
 namespace Broker
 {
-    class Program
+    class Broker
     {
         static void Main(string[] args)
         {
+            //deve receber o porto nos argumentos. Hope so...
+            TcpChannel channel = new TcpChannel(8086);
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                typeof(BrokerServices), "BrokerServices",
+                WellKnownObjectMode.Singleton);
+            System.Console.WriteLine("Press <enter> to terminate Broker...");
+            System.Console.ReadLine();
+
         }
     }
+
+    public class BrokerServices : MarshalByRefObject, BrokerInterface
+    {
+        string daddy;
+        List<string> sons;
+        Dictionary<string, List<SubscriverInterface>> subscribersByTopic;
+
+        BrokerServices(){
+
+
+        }
+
+
+        public void subscribe(string topic, string subscriberURL) {
+            Console.WriteLine("New subscriber listening at " + subscriberURL);
+            SubscriverInterface newSubscriber =
+                (SubscriverInterface)Activator.GetObject(
+                       typeof(SubscriverInterface), subscriberURL);
+
+            if (subscribersByTopic.ContainsKey(topic)) 
+                subscribersByTopic[topic].Add(newSubscriber);
+            else 
+                subscribersByTopic.Add(topic, new List<SubscriverInterface> { newSubscriber });
+            
+        }
+
+        public void unsubscribe(string topic)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
--- CommonTypes/Program.cs
+++ CommonTypes/Program.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace CommonTypes
 {
-    public interface SubscriverInterface
-    {
+    public interface SubscriverInterface{
+        //classe para o evento??
+        void deliverEvent(Event newEvent);
 
     }
 
@@ -18,6 +19,33 @@ namespace CommonTypes
 
     public interface EventReceiver
     {
+
+    }
+
+    public interface BrokerInterface{
+        void subscribe(string topic, string subscriberURL);
+        void unsubscribe(string topic);        
+    }
+
+    [Serializable]
+    public class Event{
+        private string topic;
+        private string content;
+
+        public Event(string topic, string content) {
+            this.topic = topic;
+            this.content = content;
+        }
+
+        public string Topic {
+            get { return topic; }
+            set { topic = value; }
+        }
+
+        public string Content {
+            get { return content; }
+            set { content = value; }
+        }
 
     }
 
