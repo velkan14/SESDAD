fb50af3efd80610a51e86118d98e0b1758e4fb8b
velkan14 <velkan14@gmail.com>
2015-10-28 21:58:46 +0000

    Added more classes

---
+++ Publisher/PMPublisherImpl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTypesPM;
+
+namespace Publisher
+{
+    class PMPublisherImpl : MarshalByRefObject, PMPublisher
+    {
+        public void crash()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void freeze()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void publish(int number, string topic, int interval)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void status()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void unfreeze()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Subscriber/PMSubscriberImpl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTypesPM;
+
+namespace Publisher
+{
+    class PMSubscriberImpl : MarshalByRefObject, PMSubscriber
+    {
+        public void crash()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void freeze()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void status()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void subscribe(string topic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void unfreeze()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void unsubscribe(string topic)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
