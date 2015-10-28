24d3d712a449940b85dbb55863d1002a369abf04
velkan14 <velkan14@gmail.com>
2015-10-28 22:47:14 +0000

    publisher first try

--- Publisher/PMPublisherImpl.cs
+++ Publisher/PMPublisherImpl.cs
@@ -3,25 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommonTypes;
 using CommonTypesPM;
+using System.Threading;
 
 namespace Publisher
 {
     class PMPublisherImpl : MarshalByRefObject, PMPublisher
     {
+        private PublisherInterface broker;
+        private bool freezeFlag = false;
+        private string processName;
+
+        public PMPublisherImpl(PublisherInterface broker, string processName)
+        {
+            this.broker = broker;
+            this.processName = processName;
+        }
+
         public void crash()
         {
-            throw new NotImplementedException();
+            System.Environment.Exit(1);
         }
 
         public void freeze()
         {
-            throw new NotImplementedException();
+            this.freezeFlag = true;
         }
 
         public void publish(int number, string topic, int interval)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < number; i++)
+            {
+                Event e = new Event(topic, String.Concat(processName + " :", i));
+                broker.publishEvent(e);
+                Thread.Sleep(interval);
+            }
         }
 
         public void status()
@@ -31,7 +48,7 @@ namespace Publisher
 
         public void unfreeze()
         {
-            throw new NotImplementedException();
+            this.freezeFlag = false;
         }
     }
 }
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using CommonTypes;
+using CommonTypesPM;
 
 namespace Publisher
 {
@@ -10,6 +15,14 @@ namespace Publisher
     {
         static void Main(string[] args)
         {
+            
+            string portBroker = "";
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel, false);
+            PublisherInterface broker = (PublisherInterface)Activator.GetObject(typeof(PublisherInterface), "tcp://localhost:" + portBroker + "/PublisherInterface");
+            PMPublisherImpl publisher = new PMPublisherImpl(broker, "processName");
+            RemotingServices.Marshal(publisher, "PMPublisher", typeof(PMPublisher));
+            Console.ReadLine();
         }
     }
 }
