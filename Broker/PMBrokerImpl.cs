c921b3c9e3f5e6b997cf8a1af0359f57d37a2245
velkan14 <velkan14@gmail.com>
2015-10-28 21:50:00 +0000

    Classes created on Broker

---
+++ Broker/PMBrokerImpl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTypesPM;
+
+namespace Broker
+{
+    class PMBrokerImpl : MarshalByRefObject, PMBroker
+    {
+        public void addDad(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addSon(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void crash()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void freeze()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void status()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void unfreeze()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Broker/SubscriberImpl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTypes;
+
+namespace Broker
+{
+    class SubscriberImpl : MarshalByRefObject, SubscriberInterface
+    {
+        public void deliverToSub(Event newEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
