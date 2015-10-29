948ecfa23d0b92fd72f81b74d07198fb8f2935ba
Beatriz Santos <beatriz.vbps@gmail.com>
2015-10-29 17:27:19 +0000

    PM read config file ALL CHANGED

--- PuppetMaster/Program.cs
+++ PuppetMaster/Program.cs
@@ -10,7 +10,7 @@ namespace PuppetMaster
 {
     class Program
     {
-        private const int MAX_CONFIG_NUM = 4;
+        private const int MAX_CONFIG_NUM = 9;
         private const int MAX_COMMAND_NUM= 8;
 
         static void Main(string[] args)
@@ -21,55 +21,71 @@ namespace PuppetMaster
             string[] configFile = System.IO.File.ReadAllLines(@"D:\Beatriz\Documents\IST\DAD\SESDAD\config.txt");
 
             /**
-            * Site sitename Parent sitename|none            * Process processname Is publisher |subscriber |broker On sitename URL process-url
+            * Site sitename Parent sitename|none
+            * Process processname Is publisher |subscriber |broker On sitename URL process-url
             * RoutingPolicy flooding|filter
             * Ordering NO|FIFO|TOTAL
             **/
 
             String[] configFormat = new String[MAX_CONFIG_NUM]{ "Site \\w+ Parent \\w+",
-                                            "Process \\w+ Is publisher|subscriber|broker On \\w+ URL \\w+",
-                                            "RoutingPolicy flooding|filter",
-                                            "Ordering NO|FIFO|TOTAL"
+                                            "Process \\w+ Is publisher On \\w+ URL \\w+",
+                                            "Process \\w+ Is subscriber On \\w+ URL \\w+",
+                                            "Process \\w+ Is broker On \\w+ URL \\w+",
+                                            "RoutingPolicy flooding",
+                                            "RoutingPolicy filter",
+                                            "Ordering NO",
+                                            "Ordering FIFO",
+                                            "Ordering TOTAL"
                                           };
 
             Match config;
-            int counter = 0;
-            bool anyMatch = false;
-
-            for (int configLine = 0; configLine < configFile.Length; configLine++)
-            {
-                config = Regex.Match(configFile[configLine], configFormat[counter]);
-                if (!config.Success && anyMatch)
+            for (int configFileLine = 0; configFileLine < configFile.Length; configFileLine++) {
+                for (int configFormatLine = 0; configFormatLine < MAX_CONFIG_NUM; configFormatLine++)
                 {
-                    config = Regex.Match(configFile[configLine], configFormat[++counter]);
-                    anyMatch = false;
-                    if (!config.Success)
+                    config = Regex.Match(configFile[configFileLine], configFormat[configFormatLine]);
+                    if (config.Success)
                     {
-                        Console.Write("Error: Does not match: ");
-                        Console.WriteLine(configFile[configLine]);
-                        Console.ReadLine();
-                        return;
-                    }
-                    else
-                    {
-                        Console.Write("Well formated: ");
-                        Console.WriteLine(configFile[configLine]);
-                    }
-                }
-                else if (!config.Success && !anyMatch) {
-                    Console.WriteLine("Error: There is no \" " + configFormat[configLine] + " \" line");
-                    Console.ReadLine();
-                    return;
 
-                }
-                else if (config.Success)
-                {
-                    anyMatch = true;
-                    Console.Write("Well formated: ");
-                    Console.WriteLine(configFile[configLine]);
-                }
+                        string[] splitedConfig = configFile[configFileLine].Split(' ');
 
+                        switch (configFormatLine)
+                        {
+                            case 0:
+                                //Site \\w + Parent \\w +
+                                break;
+                            case 1:
+                                //Process \\w+ Is publisher On \\w+ URL \\w+
+                                break;
+                            case 2:
+                                //Process \\w + Is subscriber On \\w + URL \\w +
+                                break;
+                            case 3:
+                                //Process \\w+ Is broker On \\w+ URL \\w+
+                                break;
+                            case 4:
+                                //RoutingPolicy flooding
+                                break;
+                            case 5:
+                                //RoutingPolicy filter
+                                break;
+                            case 6:
+                                //Ordering NO
+                                break;
+                            case 7:
+                                //Ordering FIFO
+                                break;
+                            case 8:
+                                //Ordering TOTAL
+                                break;
+                            default:
+                                Console.WriteLine("Default case");
+                                break;
+                        }
+
+                    }
+                }
             }
+
 
             Console.ReadLine();
             
