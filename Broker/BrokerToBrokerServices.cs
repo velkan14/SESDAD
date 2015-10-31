fa400154e53e29baf021a95ddcd91d6b1ece1954
António Ferreira <metal_antonio@hotmail.com>
2015-10-31 18:46:47 +0000

    adicionado BrokerToBrokerServices file

---
+++ Broker/BrokerToBrokerServices.cs
@@ -0,0 +1,41 @@
+﻿using CommonTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broker
+{
+    class BrokerToBrokerServices : MarshalByRefObject, BrokerToBrokerInterface  {
+        BrokerToBrokerInterface dad;
+        List<BrokerToBrokerInterface> sons;
+        Dictionary<string, List<SubscriberInterface>> subscribersByTopic;
+        string routing;
+
+        public BrokerToBrokerServices(BrokerToBrokerInterface dad, List<BrokerToBrokerInterface> sons, 
+            Dictionary<string, List<SubscriberInterface>> subscribersByTopic, string routing) {
+            this.dad = dad;
+            this.sons = sons;
+            this.subscribersByTopic = subscribersByTopic;
+            this.routing = routing;
+        }
+
+        public void forwardEvent(Event evt) {
+            if (routing.Equals("flooding")) {
+                if (dad != null) {
+                    dad.forwardEvent(evt);
+                }
+                foreach (BrokerToBrokerInterface son in sons) {
+                    son.forwardEvent(evt);
+                }
+                var flattenList = subscribersByTopic.SelectMany(x => x.Value);
+                foreach (SubscriberInterface sub in flattenList) {
+                    sub.deliverToSub(evt);
+                }
+            } else {
+                //TODO
+            }
+        }
+    }
+}
