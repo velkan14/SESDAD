d0339ae7341e83f5d2e01e105d2a98d05b6a48de
velkan14 <velkan14@gmail.com>
2015-11-02 18:53:23 +0000

    Async in publisher

---
+++ Publisher/Publisher.cs
@@ -0,0 +1,56 @@
+﻿using CommonTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Publisher
+    {
+        private BrokerPublishInterface broker;
+        private bool freezeFlag = false;
+        private string processName;
+        private int eventContent = 0;
+
+        public Publisher(BrokerPublishInterface broker, string processName)
+        {
+            this.broker = broker;
+            this.processName = processName;
+        }
+
+        public void crash()
+        {
+            System.Environment.Exit(1);
+        }
+
+        public void freeze()
+        {
+            freezeFlag = true;
+            Monitor.Wait(this);
+        }
+
+        public void publish(int number, string topic, int interval)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                Event e = new Event(topic, String.Concat(processName + " :", eventContent++));
+                broker.publishEvent(e);
+                Thread.Sleep(interval);
+            }
+        }
+
+        public void status()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void unfreeze()
+        {
+            freezeFlag = false;
+            //Monitor.Pulse(broker);
+        }
+    }
+}
