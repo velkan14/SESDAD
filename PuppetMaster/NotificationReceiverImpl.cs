cc3318905760994c0f9e6db809d070b06fd63302
velkan14 <velkan14@gmail.com>
2015-11-05 10:45:23 +0000

    Added NotificationReceiver

---
+++ PuppetMaster/NotificationReceiverImpl.cs
@@ -0,0 +1,43 @@
+﻿using CommonTypesPM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuppetMaster
+{
+    class NotificationReceiverImpl : MarshalByRefObject, NotificationReceiver
+    {
+        public delegate void Log(string s);
+
+        PM pm;
+
+        public NotificationReceiverImpl(PM pm) {
+            this.pm = pm;
+        }
+
+        public void publishNotification()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void forwardNotification()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void receiveNotification()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void notify(string notification)
+        {
+            Console.WriteLine(notification);
+            pm.log(notification);
+            /*Log RemoteDel = new Log(pm.log);
+            RemoteDel.BeginInvoke(notification, null, null);*/
+        }
+    }
+}
