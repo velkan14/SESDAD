da82a94aa4ea6c9d34b7082c5b1e055c550c0180
velkan14 <velkan14@gmail.com>
2015-12-04 18:01:41 +0000

    Added cenas

---
+++ Publisher/PublisherImpl.cs
@@ -0,0 +1,22 @@
+﻿using CommonTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class PublisherImpl : MarshalByRefObject, PublisherInterface
+    {
+        Publisher p;
+        public PublisherImpl(Publisher p)
+        {
+            this.p = p;
+        }
+        public void changeBroker(string newUrl)
+        {
+            p.changeBroker(newUrl);
+        }
+    }
+}
